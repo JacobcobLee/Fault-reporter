df36b39e93610c4e9cf99113d757b51f84155391
graysonlim18 <graysonlim.18@ichat.sp.edu.sg>
2020-09-12 20:21:10 +0800

    12sep2020

---
+++ build/precache-manifest.0d507647e3c06631ff8417b9ca847d64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513ba161f16ef8ca630596c6c06ef47e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1719a46425c51de5982",
+    "url": "/static/css/2.e381d3a9.chunk.css"
+  },
+  {
+    "revision": "ffa1676a974b7dc54653",
+    "url": "/static/css/main.06b77ee1.chunk.css"
+  },
+  {
+    "revision": "c1719a46425c51de5982",
+    "url": "/static/js/2.669d57d9.chunk.js"
+  },
+  {
+    "revision": "766b9957a42135e0e07cb6507f49d3c7",
+    "url": "/static/js/2.669d57d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa1676a974b7dc54653",
+    "url": "/static/js/main.243de75c.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.243de75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "d4b61703b4b0074d7a641c1117527a9a",
+    "url": "/static/media/bch_logo.d4b61703.png"
+  },
+  {
+    "revision": "76ac2c2e4ca3dc35fe765d620d30166a",
+    "url": "/static/media/bch_sidebar.76ac2c2e.jpg"
+  },
+  {
+    "revision": "db12da4e7fd7363c4603f3d1ada7711d",
+    "url": "/static/media/bg2.db12da4e.png"
+  }
+]);
